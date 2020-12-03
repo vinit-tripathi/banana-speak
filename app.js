ef4d52b2ab8493cdc37fca4f4efbc8e9d4efc120
vinit-tripathi <74803056+vinit-tripathi@users.noreply.github.com>
2020-12-03 22:37:43 +0530

    docs: elements added

---
+++ app.js
@@ -0,0 +1,12 @@
+var textInput = document.querySelector("#input-text");
+var translate = document.querySelector("#btn-translate");
+var output = document.querySelector("#output");
+// translate.addEventListener("click",function(){
+//     console.log("clicked");
+// })
+// translate.addEventListener("click",function(){
+//     console.log(textInput.value);
+// })
+translate.addEventListener("click",function(){
+console.log(output.innerText);
+})
