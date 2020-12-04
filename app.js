0e9e55e6b2818533218492f8a524b70ad1aa7792
vinit-tripathi <74803056+vinit-tripathi@users.noreply.github.com>
2020-12-04 14:54:39 +0530

    style: style merged

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-var textInput = document.querySelector("#input-text");
+var inputText = document.querySelector("#textInput");
 var translate = document.querySelector("#btn-translate");
-var output = document.querySelector("#output");
+var output = document.querySelector("#outputDiv");
 // translate.addEventListener("click",function(){
 //     console.log("clicked");
 // })
 // translate.addEventListener("click",function(){
 //     console.log(textInput.value);
 // })
-translate.addEventListener("click",function(){
-console.log(output.innerText);
-})
+// translate.addEventListener("click",function(){
+// console.log(output.innerText);
+// })
