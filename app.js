4e8fcf1c9110e3f3f5ff12313cf5335ab0e47746
vinit-tripathi <74803056+vinit-tripathi@users.noreply.github.com>
2020-12-05 21:16:10 +0530

    style: border radius updated

--- app.js
+++ app.js
@@ -4,22 +4,24 @@ var outputDiv = document.querySelector("#output-Div");
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 var serverURL = "https://api.funtranslations.com/translate/minion.json"
 
-function getTranslateURL(text){
-return serverURL + "?" + "text=" + text
+function getTranslateURL(text) {
+    return serverURL + "?" + "text=" + text
 }
-function errorHandler(error){
+
+function errorHandler(error) {
     console.log("error occured", error);
     alert("something went wrong with server! Try again after sometime")
 }
+
 function clickHandler() {
     var inputText = textInput.value;
     fetch(getTranslateURL(inputText))
-    .then(response => response.json())
-    .then(json => {
-        var translatedText = json.contents.translated;
-        outputDiv.innerText = translatedText;
-    })
-    .catch(errorHandler)
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            outputDiv.innerText = translatedText;
+        })
+        .catch(errorHandler)
 };
 
 
